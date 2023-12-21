710013a9f8baa7e748281c5b05bb68fc6da5c6a5
liuyao <798840120@qq.com>
2023-12-21 11:16:35 +0800

    issue: 增加文档

---
+++ spring_cloud_learn/src/main/java/org/example/Application.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application:
+ *
+ * @author: 刘瑶
+ * @description:
+ * @date: 2023/12/21
+ * @version: 1.0.0
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ spring_cloud_learn/src/main/resources/doc/api安全.md
@@ -0,0 +1,12 @@
+## api 信息安全
+
+![img_2.png](img_2.png)
+
+![img_3.png](img_3.png)
+
+### 风险与安全机制：
+- 风险
+![img_1.png](img_1.png)
+- 安全机制
+![img.png](img.png)
+![img_4.png](img_4.png)
